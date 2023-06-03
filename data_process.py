5e9daf6c4fbf8bd3924c13549b0df35f1a9a3d5f
gong7788 <g827029400@gmail.com>
2023-06-03 21:44:03 +0100

    demo-summary

---
+++ data_process.py
@@ -0,0 +1,34 @@
+import json
+
+data_path = "train1.json"
+
+with open(data_path, 'r') as f:
+    data = json.load(f)
+
+def clean_data(text):
+    text = text.replace('{vocalsound}', '')
+    text = text.replace('{disfmarker}', '')
+    text = text.replace('a_m_i_', 'ami')
+    text = text.replace('l_c_d_', 'lcd')
+    text = text.replace('p_m_s', 'pms')
+    text = text.replace('t_v_', 'tv')
+    text = text.replace('{pause}', '')
+    text = text.replace('{nonvocalsound}', '')
+    text = text.replace('{gap}', '')
+    return text
+
+def extract_text_from_json(json_data):
+    data = []
+    for turn, meeting in enumerate(json_data['meeting_transcripts']):
+        if meeting:
+            data.append({
+                'turn': turn,
+                'speaker': meeting['speaker'],
+                'content': clean_data(meeting['content']),
+            })
+    return data
+
+temp = extract_text_from_json(data)
+
+with open("data.json", "w") as f:
+    json.dump(temp, f)
