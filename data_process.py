e1a5dfc17d99c0a4946db07f8f320dbb7585e5cc
gong7788 <g827029400@gmail.com>
2023-06-08 20:20:39 +0100

    test

--- data_process.py
+++ data_process.py
@@ -6,13 +6,13 @@ with open(data_path, 'r') as f:
     data = json.load(f)
 
 def clean_data(text):
-    #TODO: T_V_ -> TV
     text = text.replace('{vocalsound}', '')
     text = text.replace('{disfmarker}', '')
     text = text.replace('a_m_i_', 'ami')
     text = text.replace('l_c_d_', 'lcd')
     text = text.replace('p_m_s', 'pms')
     text = text.replace('t_v_', 'tv')
+    text = text.replace('T_V_', 'TV')
     text = text.replace('{pause}', '')
     text = text.replace('{nonvocalsound}', '')
     text = text.replace('{gap}', '')
